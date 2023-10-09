7879e2e48117bc582cfe98e46270bcb01ae9e4b3
Alfonso1025 <alfonso25elorriaga@gmail.com>
2023-10-09 17:55:36 -0500

    testing if hiting chatbot rout can read api key

--- chatbot.js
+++ chatbot.js
@@ -7,57 +7,84 @@ const openai = new api( new config({
     apiKey: key
 }))
 
-const about = `Hello, my name is Alfonso and I m a Full Stack developer from Chicago. I am fluent in both English and Spanish and have been developing for the past 3 years. Over this time, I have completed more than 7 projects, gaining expertise in various programming languages, frameworks, and tools.Front-end skills:
-I am proficient in front-end technologies such as HTML, CSS, and JavaScript, and have developed mobile apps using React Native and Node.js. With the help of AI, I have also designed user interfaces for some apps, making use of my creative and analytical skills.
+const about = `I m a Full Stack developer from Chicago. I am fluent in both English and Spanish 
+and have been developing software for over  3 years. Over this time, I have 
+completed  multiple projects in the following technologies:
+ 1. Reverse proxies with nginx.
 
-Back-end skills:
-I specialize in the backend of the development stack and my favorite programming language is C. However, I am proficient in other programming languages like C++, and JavaScript. I have also worked with frameworks such as React and Node.js with Express.js. I can use Prop Engineering to enhance the efficiency of the backend and I have used this technology to create a personal assistant for myself.
+ 2. Front end
+ a)html
+ b)css
+ c)vanilla javascript
+ d)React.js
 
-Database skills:
-I have experience working with both SQL and NoSQL databases. I am proficient in SQL databases like PostgreSQL, MySQL, and others. Additionally, I have worked with NoSQL databases like MongoDB and Mongoose, gaining expertise in data modeling and database management.
+ 3.Backend
+ a)node.js
+ b)express.js
+ c)sql databases such as: Mysql and Postgres
+ d)noSql databases sucha as Mongodb
+ f)Database design using uml diagrams for model entity-relanshionship
+ g)unit testing
+ h)api testing
+ i)Doumenting using swagger and mark down
+ j)c4 IBM model
 
-Version control:
-I am well-versed in using Git and GitHub for version control, and I have utilized these tools in my projects to maintain version history and track changes.
+4.Version controll
+a)gitbash
+b)github
 
-Deployment skills:
-I am experienced in deploying apps to AWS and have deployed applications to EC2 instances, S3, and Amplify. Additionally, I have deployed databases to RDS instances, and I am proficient in the AWS environment.
+5. Deployment and cloud services
+a)ec2 instances in aws
+b)s3 buckets
+c)amazon rds with mysql and postgres databases
 
-In summary, I am a Full Stack developer with expertise in front-end and back-end development, database management, version control, and deployment skills. I am always eager to learn and explore new technologies, and I m confident in my ability to contribute meaningfully to any project I work on.`
+6. Linux distribution consoles
+
+7. c, c++ adn python programming languages. 
+ 
+I went to law school and worked in the legal field for a few years. I pivoted carees towards 
+software development.
+I am a self taught software engeenier. I learned over years by attending online and in person curses, self study, private tutoring and building practical projects
+
+`
 
 const contact = ` email:  alfonso25elorriaga@gmail.com
                     phone number: 773 991 7798 `
+
 const context = [
-    {role: 'system', content: `You are a friendly AI assistante for my professional software deveopment portfolio. The purpose
+    {role: 'system', content: ` Restrict your answer to 100 characters. You are a friendly AI assistante for my professional software development portfolio. The purpose
     of the portfolio is to help me get a job a as software developer/ engineer/programmer. 
     Or get hired as freelancer for a software development project. Your job is to assit the visitor 
     of my portfolio by greeting them and responding to their questions and comments regarding my 
     professional profile. You can get the information about my professional profile in the "about"
     text delimited by triple medium dashes "--".
+    Your responses should not be longer than 100 characters. 
     If the user makes a question or commment that is not regarding my professional profile, 
-    then respond, in your own words, that you can only awnser questions about my professional 
+    then respond, in your own words, that you can only answer questions about my professional 
     profile and do not answer the question. Also  respond why the question is  unrelated to my proffesional profile. 
     This is an example of how to handle this case:
     user: Tell me about the second world war
     assistant: Sorry that question is not related to Alfonso professional profile. 
-    If the question is related to my professional profile answer it but do not mention 
+    If the question is related to my professional profile, answer it but do not mention 
     why is related to my professional profile.
     This is an example of how to handke this case:
     user: can he program in c++?
-    assistant: Yes, Alfonso has work in multiple projects using  c++.
+    assistant: Yes, Alfonso has worked in multiple projects using  c++.
+    In this portfolio, there is an About section where the users can read more aboit my skills.
+    Also, there is a projects section, where users can see some of my projects. 
     If the user wants to contact me to set up an interview or to get to know me better or to talk
     to me directly or leave a message or something similar, offer 2 options: 
     1. give them my contact info, use the info in the "contact" delimited by slashes. 
-    2. offer to take their message so you will contact me on behalf of the user. Ask for his/her info
-    such as phone number, email, name company and subject. Tell the user that I will get back at him/her
-    as soon as I can. 
+    2. offer the user to go to the contact page in the portafolio. The user can find the link
+     to it in the navigation bar on top of the screen
     This is an example of how to handke this case:
     user: I would like to interview him
-    assistant: Great I can take you details such as number, name or email and and let Alfonso 
-    know that you are intersted on an interview. Or this I can give you his contact details 
-    and you can contact him yourself.
+    assistant:  I can give you his contact details or you could go to the conctact section 
+    and leave you message and details in the contact form
+    
   
     Always thank the user for visiting my portfolio and for his/her interest. 
-    Restrict your answer to no more than 150 characters
+    Restrict your answer to no more than 100 characters
     about: ---${about}---.
     contact: ///${contact}/// `}
 ]
@@ -79,8 +106,10 @@ module.exports = {
   
   collectMessages : async (req, res)=>{
     const resolver = Resolver(res)
-    console.log(req.body.message)
-    try {
+    console.log('this is the message',req.body.message)
+    console.log('this is the key', key)
+
+    /* try {
        const prompt = req.body.message
        context.push({role : 'user', content: prompt})
        const response = await getCompletionFromMessages(context)
@@ -88,9 +117,9 @@ module.exports = {
        console.log(response)
        return resolver.success(response, 'messages_collected')
   } catch (error) {
-       console.log(error)
+       console.log(error.message)
        return resolver.internalServerError(error, error.message)
-  }
+  } */
    
   }
 }
